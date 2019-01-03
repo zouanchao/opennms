7d2dbc40dccf8d59d3e3e0bc0c0badb6ed75bacf
David Smith <smith@opennms.com>
2019-01-03 12:26:25 -0500

    NMS-10523: Set the year so test messages are not in the future

--- features/events/syslog/src/test/java/org/opennms/netmgt/syslogd/ConvertToEventTest.java
+++ features/events/syslog/src/test/java/org/opennms/netmgt/syslogd/ConvertToEventTest.java
@@ -535,8 +535,7 @@ public class ConvertToEventTest {
                 .getContent());
         assertEquals(componentId, event.getParm("ComponentId").getValue()
                 .getContent());
-        Date expectedDate = new SimpleDateFormat("yyyy MMM dd hh:mm:ss").parse(Calendar.getInstance()
-                .get(Calendar.YEAR) + " " + date);
+        Date expectedDate = new SimpleDateFormat("yyyy MMM dd hh:mm:ss").parse(SyslogMessageTest.getExpectedYear(date) + " " + date);
         assertEquals(expectedDate, event.getTime());
         assertEquals(processName, event.getParm("process").getValue().getContent());
         assertEquals(processId, event.getParm("processid").getValue().getContent());
@@ -561,8 +560,7 @@ public class ConvertToEventTest {
                 event.getParm("severity").getValue().getContent());
         assertEquals(sequenceNum, event.getParm("SequenceNum").getValue()
                 .getContent());
-        Date expectedDate = new SimpleDateFormat("yyyy MMM dd hh:mm:ss").parse(Calendar.getInstance()
-                .get(Calendar.YEAR) + " " + date);
+        Date expectedDate = new SimpleDateFormat("yyyy MMM dd hh:mm:ss").parse(SyslogMessageTest.getExpectedYear(date) + " " + date);
         assertEquals(expectedDate, event.getTime());
         assertEquals(messageContent, event.getLogmsg().getContent());
     }
